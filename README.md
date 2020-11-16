9fdc54891ccafe4373d5525c2aeafd2f396593c8
fryderyk <fryderyk.entwicklung@outlook.com>
2020-11-16 11:54:09 +0100

    RESTFULL API

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 REST API
 What is REST API?
+
 It stands for : 
+
 ## Resprentational State Transfere
+
+CLIENT -> REQUEST -> SERVER
+SERVER ->  RESPONS -> CLIENT
+
---
+++ node-rest-shop/server.js
@@ -0,0 +1,8 @@
+// For my NODE.JS SERVER
+
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port);
